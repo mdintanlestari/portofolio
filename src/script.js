2ff62ff0f88d09c5663dd49a8465f3486f09cd1a
Intan <intanlestari2130@gmail.com>
2025-04-01 21:59:46 +0800

    upload project

---
+++ src/script.js
@@ -0,0 +1,15 @@
+<!-- home -->
+<div class="bg-gray-100 pt-40">
+  <section id="home" class="text-center space-y-16">
+    <h1 class="text-5xl font-extrabold">HEY, I'M NI MADE INTAN LESTARI</h1>
+    <p class="font-medium text-2xl">
+      I am learning and developing skills as a Front-End Developer by
+      exploring HTML, CSS, and JavaScript.
+    </p>
+    <button
+      class="bg-blue-800 hover:bg-blue-950 rounded text-white font-medium text-xl w-52 h-16"
+    >
+      <a href="">PROJECTS</a>
+    </button>
+  </section>
+</div>
